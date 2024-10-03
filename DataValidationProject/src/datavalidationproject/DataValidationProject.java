3dd41562ba93a7af7b239acf68684e360dae45ab
ThatoMavuso <131204370+ThatoMavuso@users.noreply.github.com>
2024-10-03 22:21:32 +0200

    Add files via upload

---
+++ DataValidationProject/src/datavalidationproject/DataValidationProject.java
@@ -0,0 +1,16 @@
+
+package datavalidationproject;
+
+
+public class DataValidationProject {
+
+   
+    public static void main(String[] args) {
+        LoginPage();
+    }
+
+    private static void LoginPage() {
+      
+    }
+    
+}
